0112267259c904ed75015d98e0c2e591439c60b5
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-14 21:53:05 -0800

    Update Flag Emoji

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ import requests
 from rich import print
 
 
+print("🏳️ Starting Cloudflare Purge Cache Action")
+print("🏳 Starting Cloudflare Purge Cache Action")
 print(":white_flag: Starting Cloudflare Purge Cache Action")
+print(":white_flag-emoji: Starting Cloudflare Purge Cache Action")
+print(":white_flag-text: Starting Cloudflare Purge Cache Action")
 
 
 # Inputs
